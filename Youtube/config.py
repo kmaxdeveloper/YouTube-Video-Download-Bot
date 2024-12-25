aaee064fe87239035755cc8d3b49208bee95eb8b
kmaxdeveloper <100609343+kmaxdeveloper@users.noreply.github.com>
2024-12-25 20:46:49 +0500

    Update config.py

--- Youtube/config.py
+++ Youtube/config.py
@@ -2,9 +2,9 @@ import os
 
 class Config(object):
      
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8080910166:AAFxAxoUoaHyTouqlAAbva00908zw2ARGKs")
+    API_ID = int(os.environ.get("API_ID",28545775))
+    API_HASH = os.environ.get("API_HASH", "f6fc44cfb5b41d81531a9abe22194aad")
     #Add your channel id. For force Subscribe.
     CHANNEL = os.environ.get("CHANNEL", "")
     #Skip or add your proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
